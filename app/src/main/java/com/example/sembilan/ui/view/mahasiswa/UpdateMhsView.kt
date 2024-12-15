6783936e2e931ccc99e171a8bf17bad0beb96463
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-15 21:48:42 +0700

    membuat parameter dari fungsi UpdateMhsView

--- app/src/main/java/com/example/sembilan/ui/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/sembilan/ui/view/mahasiswa/UpdateMhsView.kt
@@ -1,8 +1,18 @@
 package com.example.sembilan.ui.view.mahasiswa
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.sembilan.ui.viewModel.PenyediaViewModel
+import com.example.sembilan.ui.viewModel.UpdateMhsViewModel
 
 @Composable
-fun UpdateMhsView() {
+fun UpdateMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory) //Inisialisasi ViewModel
+) {
+
 
 }
