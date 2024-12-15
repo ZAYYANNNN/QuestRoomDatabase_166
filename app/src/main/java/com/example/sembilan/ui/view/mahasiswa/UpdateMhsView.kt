1d501c86d50f16b4ff741d7a304a5b81af8d9ff1
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-15 21:51:27 +0700

    membuat variabel dari fungsi UpdateMhsView

--- app/src/main/java/com/example/sembilan/ui/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/sembilan/ui/view/mahasiswa/UpdateMhsView.kt
@@ -1,10 +1,14 @@
 package com.example.sembilan.ui.view.mahasiswa
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sembilan.ui.viewModel.PenyediaViewModel
 import com.example.sembilan.ui.viewModel.UpdateMhsViewModel
+import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun UpdateMhsView(
@@ -13,6 +17,7 @@ fun UpdateMhsView(
     modifier: Modifier = Modifier,
     viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory) //Inisialisasi ViewModel
 ) {
-
-
+    val uiState = viewModel.updateUIState // ambil uistate dari viewmodel
+    val snackbarHostState = remember { SnackbarHostState() } // snackbar state
+    val coroutineScope = rememberCoroutineScope()
 }
