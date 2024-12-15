d87dd64db067cc8f70631dcb621af05cbbb4051d
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-15 20:23:28 +0700

    menambahkan parameter fungsi deleteconfirmationdialog

--- app/src/main/java/com/example/sembilan/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/sembilan/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,10 +1,13 @@
 package com.example.sembilan.ui.view.mahasiswa
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 
 @Composable
 private fun DeleteConfirmationDialog(
-
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
 ){
+
 
 }
