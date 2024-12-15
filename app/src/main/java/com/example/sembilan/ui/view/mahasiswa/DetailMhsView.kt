6a06825e18df4c42990946fca7c56966669cb24e
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-15 20:35:15 +0700

    membuat alertdialog, dismissbutton dan confirmbutton

--- app/src/main/java/com/example/sembilan/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/sembilan/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,5 +1,10 @@
 package com.example.sembilan.ui.view.mahasiswa
 
+import android.R.attr.text
+import android.R.attr.title
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -8,6 +13,19 @@ import androidx.compose.ui.Modifier
 private fun DeleteConfirmationDialog(
     onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
 ){
-
+    AlertDialog(onDismissRequest = { /* Do Nothing */}),
+    title = { Text("Delete Data") },
+    text = {Text("Apakah Anda Yakin Ingin Menghapus Data?") },
+    modifier = modifier,
+    dismissButton = {
+        TextButton(onClick = onDeleteCancel) {
+            Text(text = "Cancel")
+        }
+    },
+    confirmButton = {
+        TextButton(onClick = onDeleteConfirm) {
+            Text(text = "Yes")
+        }
+    }
 
 }
