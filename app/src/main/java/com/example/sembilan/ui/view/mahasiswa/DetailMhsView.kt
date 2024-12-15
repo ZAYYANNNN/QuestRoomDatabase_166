d0a0fa69f1c23cde253af760f774da82d5ccae8b
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-15 21:10:44 +0700

    membuat variabel dan box

--- app/src/main/java/com/example/sembilan/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/sembilan/ui/view/mahasiswa/DetailMhsView.kt
@@ -2,17 +2,24 @@ package com.example.sembilan.ui.view.mahasiswa
 
 import android.R.attr.text
 import android.R.attr.title
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,6 +37,18 @@ fun BodyDetailMhs(
     onDeleteClick: () -> Unit = { }
 
 ) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator() // Tampilkan Loading
+            }
+        }
+
+    }
 
 }
 @Composable
